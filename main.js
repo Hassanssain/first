17f651fb0dd095b0b8099b685f660049ae666c9f
Hassanssain <mh268171@gmail.com>
2024-12-31 12:25:02 +0500

    first

---
+++ main.js
@@ -0,0 +1,38 @@
+let items = [];
+
+
+function add(item) {
+    if (!items.includes(item)) {
+        items.push(item);
+    }
+}
+
+
+function remove(item) {
+    const index = items.indexOf(item);
+    if (index !== -1) {
+        items.splice(index, 1);
+    }
+}
+
+
+function lastChar(str) {
+    if (str.length === 0) return null; 
+    return str[str.length - 1];
+}
+
+
+function reverse(str) {
+    return str.split('').reverse().join('');
+}
+
+
+add('apples');
+add('bananas');
+console.log(items); 
+remove('apples');
+console.log(items); 
+
+console.log(lastChar('JavaScript')); 
+console.log(reverse('JavaScript')); 
+console.log(reverse(reverse('JavaScript'))); 
